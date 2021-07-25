ca7bbbd74a6db5743f2b26028992615ad4cc12b8
Calin Catuta <77282302+CalinCatuta@users.noreply.github.com>
2021-07-25 22:53:51 +0300

    Add files via upload

---
+++ Portofolio-site/Travel-Website/owl-destination.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay: true,
+        margin:160,
+        nav:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            890:{
+                items:2,
+                nav:false
+            },
+            1500:{
+                items:3,
+                nav:false
+            },
+            2000:{
+                items:4,
+                nav:false
+            }
+        }
+    })
+})
+// lightbox
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(images => {
+    images.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = images.src
+        while(lightbox.firstChild){
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if(e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
